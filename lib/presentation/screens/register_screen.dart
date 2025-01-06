e5d1701f9360d3d6b9daa499c101702e280bad67
Max-500 <213497@ids.upchiapas.edu.mx>
2025-01-05 20:26:07 -0600

    traditional form

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -27,6 +27,7 @@ class _RegisterView extends StatelessWidget {
           child: Column(
             children: [
               const FlutterLogo(size: 100),
+              const SizedBox(height: 25),
               _RegisterForm(),
               const SizedBox(height: 20)
             ],
@@ -37,10 +38,15 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
 class _RegisterForm extends StatelessWidget {
   _RegisterForm();
 
   final formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
 
   @override
   Widget build(BuildContext context) {
@@ -49,19 +55,64 @@ class _RegisterForm extends StatelessWidget {
       key: formKey,
       child: Column(
       children: [
-          CustomTextFormField(label: 'Username', hintText: 'Enter Your Username',),
+          CustomTextFormField(
+            label: 'Username', 
+            hintText: 'Enter Your Username', 
+            onChanged: (value) => username = value,
+            validator: (value) {
+              if(value == null || value.trim().isEmpty) return 'You must write your username';
+              if(value.trim().length < 3) return 'Your name must be a minimum of 3 characters';
+              return null;
+            },
+          ),
           const SizedBox(height: 15),
-          CustomTextFormField(label: 'Email', hintText: 'Enter Your Email',),
+          CustomTextFormField(
+            label: 'Email', 
+            hintText: 'Enter Your Email',
+            onChanged: (value) => email = value,
+            validator: (value) {
+              if(value == null || value.trim().isEmpty) return 'You must write your username';
+
+              final emailRegExp = RegExp(
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+              if(!emailRegExp.hasMatch(value)) return 'Your email is not in the correct format';
+              return null;
+            },
+          ),
           const SizedBox(height: 15),
-          CustomTextFormField(label: 'Password', hintText: 'Enter Your Username', isObscure: true,),
+          CustomTextFormField(
+            label: 'Password', 
+            hintText: 'Enter Your Username', 
+            isObscure: true,
+            onChanged: (value) => password = value,
+            validator: (value) {
+              if(value == null || value.trim().isEmpty) return 'You must write your username';
+              if(value.trim().length < 8) return 'Your password must be a minimum of 8 characters';
+              return null;
+            },
+          ),
           const SizedBox(height: 15),
-          CustomTextFormField(label: 'Confirm Password', hintText: 'Enter Your Username', isObscure: true,),
+          CustomTextFormField(
+            label: 'Confirm Password', 
+            hintText: 'Enter Your Username', 
+            isObscure: true,
+            onChanged: (value) => confirmPassword = value,
+            validator: (value) {
+              if(value == null || value.trim().isEmpty) return 'You must write your username';
+              if(value.trim().length < 8) return 'Your password must be a minimum of 8 characters';
+              if(value != password) return 'Your passwords must be the same';
+              return null;
+            },
+          ),
 
           const SizedBox(height: 15),
 
           FilledButton.tonalIcon(
             onPressed: (){
+              final isValid = formKey.currentState!.validate();
 
+              if(!isValid) return;
             },
             icon: const Icon(Icons.save_rounded), 
             label: const Text('Save User')
